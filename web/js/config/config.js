4eb24e8bd3d71ba2906cca994b22c8ee490ccc76
Naximls <93773088+NaximL@users.noreply.github.com>
2025-06-21 22:44:39 +0300

    Add files via upload

--- web/js/config/config.js
+++ web/js/config/config.js
@@ -1,55 +1,53 @@
-
-
 export const skins = {
-    normal: '../imgs/akula_new.png',
-    time: '../imgs/Skins/time.png',
-    akula_nohsnki: '../imgs/Skins/akula_nohsnki.png',
-    akula_nohsnki_shapka: '../imgs/Skins/akula_nohsnki_shapka.png',
-    crismas: '../imgs/Skins/crismas.png',
-    monokl: '../imgs/Skins/monokl.png',
-    paznik: '../imgs/Skins/paznik.png',
-    plach: '../imgs/Skins/plach.png',
+    normal: '/imgs/akula_new.png',
+    time: '/imgs/Skins/time.png',
+    akula_nohsnki: '/imgs/Skins/akula_nohsnki.png',
+    akula_nohsnki_shapka: '/imgs/Skins/akula_nohsnki_shapka.png',
+    crismas: '/imgs/Skins/crismas.png',
+    monokl: '/imgs/Skins/monokl.png',
+    paznik: '/imgs/Skins/paznik.png',
+    plach: '/imgs/Skins/plach.png',
+};
 
-}
 export const trash = [
     {
         name: "banka",
-        img: "../imgs/trash/banka.png",
+        img: "/imgs/trash/banka.png",
         scope: 3,
-        chance:2,
-        scale:1.5,
+        chance: 2,
+        scale: 1.5,
     },
     {
         name: "bankas",
-        img: "../imgs/trash/bankas.png",
+        img: "/imgs/trash/bankas.png",
         scope: 2,
         chance: 33,
-        scale:1
+        scale: 1
     },
     {
         name: "steklo",
-        img: "../imgs/trash/steklo.png",
+        img: "/imgs/trash/steklo.png",
         scope: 1,
         chance: 30,
-        scale:1
+        scale: 1
     },
     {
         name: "banan",
-        img: "../imgs/trash/banan.png",
+        img: "/imgs/trash/banan.png",
         scope: 1,
         chance: 20,
-        scale:1
+        scale: 1
     },
     {
         name: "ghestanka",
-        img: "../imgs/trash/ghestanka.png",
+        img: "/imgs/trash/ghestanka.png",
         scope: 5,
         chance: 15,
-        scale:1
+        scale: 1
     }
 ];
 
-export const scale = [0.5, 1, 1.5]
+export const scale = [0.5, 1, 1.5];
 
 export let mapWidths = 1500;
 export let mapHeights = 1500;
